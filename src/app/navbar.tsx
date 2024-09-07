6d11abdf683ee6520c55ad1cf9164885a7cf0802
michaelzoub <micacao15@gmail.com>
2024-09-07 17:02:12 -0400

    added mq for mobile

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -10,6 +10,27 @@ import { useState } from "react";
 export function Navbar() {
     const [languagearrow, setLanguagearrow] = useState(false)
     const [loginClick, setLoginClick] = useState(false)
+    const [sidebar, setSidebar] = useState(false)
+    const [showBar, setShowBar] = useState(false)
+
+    const [heightCheck, setHeightCheck] = useState(false)
+
+    function sideBar() {
+      if (sidebar) {
+        setHeightCheck(false)
+        setShowBar((e) => !e)
+        setTimeout(()=> {
+          setSidebar((e) => !e)
+        }, 400)
+      } else {
+      setHeightCheck(true)
+      setSidebar((e) => !e)
+      setTimeout(()=> {
+        setShowBar((e) => !e)
+      }, 300);
+    }
+    }
+
 
     function languageClick() {
         languagearrow ? setLanguagearrow(false) : setLanguagearrow(true)
@@ -20,7 +41,9 @@ export function Navbar() {
       setLoginClick(true)
     }
     return (
-        <main className="absolute flex flex-row justify-between w-full h-16 justifiy-center text-white p-2">
+        <main className={`${sidebar? 'absolute flex flex-row justifiy-center overflow-hidden text-white w-full h-screen md:h-16' : `absolute flex flex-row justifiy-center overflow-hidden text-white w-full ${heightCheck ? 'h-screen' : 'h-16'} md:h-16`}`}>
+          <div className="hidden md:w-full">
+          <div className="hidden md:flex flex-row h-16 w-full justify-between">
           <div className="flex flex-row">
           <div className="flex flex-row ml-14 text-lg my-auto hover:cursor-pointer font-bold">d<span className="text-red-500">lock</span>.shop</div>
           <div className="flex flex-row ml-10 my-auto hover:text-red-400 hover:cursor-pointer">Market</div>
@@ -37,6 +60,23 @@ export function Navbar() {
                 <div className="flex flex-row"><Image className="mx-2 my-auto" src={spanish} alt='spanish' width={23} height={25}></Image>Spanish</div>
             </div>
             <button onClick={loginClickHandle} className="flex mr-4 px-6 pr-8 rounded-2xl redaccent h-7 text-sm my-auto"><div className="flex flex-row my-auto"><Image className="mx-1" src={steam} alt='steam' width={20} height={20}></Image>LOGIN</div></button>
+            </div>
+            </div>
+            </div>
+
+            <div className={`${sidebar? 'flex w-full flex-row justify-end h-screen md:hidden' : 'transition delay-150 flex w-full h-0 flex-row justify-end md:hidden'}`}>
+              <div className="flex flex-col w-full">
+              <button className={`${sidebar? 'z-20 absolute end-0 text-end m-4 text-red-400' : 'absolute end-0 text-end m-4'}`} onClick={sideBar}>═</button>
+              <div className={`${sidebar? 'z-10 flex flex-row h-screen justify-end w-full transition ease-in-out delay-150 backdrop-blur' : 'navbarblur'}`}>
+              <div className={`${showBar? 'z-10 flex flex-col gap-4 animate-show w-[40%] tradebox h-screen pt-14 text-left px-4' : 'hiddenLogin flex flex-col gap-4 w-[40%] h-screen tradebox pt-14 px-4'}`}>
+                <div className="mx-auto flex flex-row text-xl hover:cursor-pointer font-bold">d<span className="text-red-500">lock</span>.shop</div>
+                <button onClick={loginClickHandle} className="flex px-6 pr-8 mb-6 rounded-md shadow-inner redaccent h-7 text-sm"><div className="flex flex-row my-auto mx-auto"><Image className="mx-1" src={steam} alt='steam' width={20} height={20}></Image>LOGIN</div></button>
+                <div className="border-2 border-red-400 p-1 pl-3 rounded-md hover:text-red-400 hover:cursor-pointer">Market</div>
+                <div className="border-2 border-red-400 p-1 pl-3 rounded-md hover:text-red-400 hover:cursor-pointer">Trade</div>
+                <div className="border-2 border-red-400 p-1 pl-3 rounded-md hover:text-red-400 hover:cursor-pointer">FAQ</div>
+              </div>
+              </div>
+            </div>
             </div>
         </main> 
       );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 //this array would have ITEM name, image url, condition etc, price and add cart function
 const array = ['','','','','','','','','','','','','','','','','','','','','','','','','']
 const heroes = ['Shiv','Seven','McGinnis']
-const fakeItems = [{id:0, price: 1, hero: 'Shiv', imgurl: 'https://community.akamai.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFQznaKdID5D6d23ldHSwKOmZeyEz21XvZZ12LzE9t6nigbgqkplNjihJIaLMlhpF1ZeR5c/360fx360f', rarity: 'common'}, {id:1, price: 1000, hero: 'Seven', imgurl: 'https://community.akamai.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsQ1xmLBcF5uj2FBdy3P7HTjlH09G_hoGMkrmkNuODwG8F7ZMl2bqSoI_22ATg_0s6a2qiIofDdA5rNVmG8la5k7i6m9bi60_Jt_x9/360fx360f', rarity: 'common'}, {id:2, price: 2, hero: 'shiv', imgurl: 'https://community.akamai.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFQznaKdID5D6d23ldHSwKOmZeyEz21XvZZ12LzE9t6nigbgqkplNjihJIaLMlhpF1ZeR5c/360fx360f', rarity: 'common'}]
+const fakeItems = [{id:0, price: 1, hero: 'Shiv', imgurl: 'https://community.akamai.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFQznaKdID5D6d23ldHSwKOmZeyEz21XvZZ12LzE9t6nigbgqkplNjihJIaLMlhpF1ZeR5c/360fx360f', rarity: 'common'}, {id:1, price: 1000, hero: 'Seven', imgurl: 'https://community.akamai.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsQ1xmLBcF5uj2FBdy3P7HTjlH09G_hoGMkrmkNuODwG8F7ZMl2bqSoI_22ATg_0s6a2qiIofDdA5rNVmG8la5k7i6m9bi60_Jt_x9/360fx360f', rarity: 'common'}, {id:2, price: 2, hero: 'shiv', imgurl: 'https://community.akamai.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFQznaKdID5D6d23ldHSwKOmZeyEz21XvZZ12LzE9t6nigbgqkplNjihJIaLMlhpF1ZeR5c/360fx360f', rarity: 'common'}, {id:0, price: 1, hero: 'Shiv', imgurl: 'https://community.akamai.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFQznaKdID5D6d23ldHSwKOmZeyEz21XvZZ12LzE9t6nigbgqkplNjihJIaLMlhpF1ZeR5c/360fx360f', rarity: 'common'}, {id:1, price: 1000, hero: 'Seven', imgurl: 'https://community.akamai.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsQ1xmLBcF5uj2FBdy3P7HTjlH09G_hoGMkrmkNuODwG8F7ZMl2bqSoI_22ATg_0s6a2qiIofDdA5rNVmG8la5k7i6m9bi60_Jt_x9/360fx360f', rarity: 'common'}, {id:2, price: 2, hero: 'shiv', imgurl: 'https://community.akamai.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsUFJ5KBFZv668FFQznaKdID5D6d23ldHSwKOmZeyEz21XvZZ12LzE9t6nigbgqkplNjihJIaLMlhpF1ZeR5c/360fx360f', rarity: 'common'}]
 
 let temp: any = [] //this is where filters are stored until reload
 
@@ -18,13 +18,15 @@ export default function Home() {
   const [filterPlus, setFilterplus] = useState(false)
   const [filterRarity, setFilterRarity] = useState(false)
   const [addCartButtonColor, setAddCartButtonColor] = useState(null)
+  const [signed, setSigned] = useState(false)
 
   const [checkedHeroes, setCheckedHeroes] = useState<string[]>([])
   const [checkedGeneral, setCheckedGeneral] = useState(false)
 
   const [inCart, setInCart] = useState<string[]>([])
 
-  const [pricerange, setPricerange] = useState(0)
+  const [smallRange, setSmallRange] = useState<number>(0)
+  const [bigRange, setBigRange] = useState<number>(0)
 
   function filterFunc() {
     filter ? setFilter(false) : setFilter(true)
@@ -58,6 +60,11 @@ export default function Home() {
     hero: checkedHeroes
   }
 
+  async function signedIn() {
+    //verify if user is signed in through cookies
+    setSigned(true)
+  }
+
   async function handleSendToApiFilter() {
     console.log(checkedHeroes)
     const response = await fetch('http://localhost:8080/api/filterFunc', {
@@ -84,25 +91,14 @@ export default function Home() {
 
   function zero(e: any) {
     //check tomorrow (drunk)
-    setPricerange(e.target.value)
-    console.log('price range:',pricerange)
-    console.log(fakeItems.filter((test:any) => test.price > pricerange))
-    if (fakeItems.find((test:any) => test.price < pricerange)) {
-      console.log('under target price')
-    } else {
-      console.log('in the bounds')
-      let test = fakeItems.filter((test:any) => test.price > pricerange)
-      console.log('test:', test)
-      let test2 = test.map((e) => e.hero)
-      console.log('for each try:', test.map((e) => e.hero))
-      setCheckedGeneral(true)
-      setCheckedHeroes(test2)
-      console.log('checked heroes:',checkedHeroes)
-    }
+    setSmallRange(e.target.value)
+    console.log(smallRange)
   }
 
-  function infinity() {
+  function infinity(e:any) {
 //copy fixed code above and inverse
+setBigRange(e.target.value)
+console.log(bigRange)
   }
 
   function itemSearch() {
@@ -110,32 +106,35 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-row items-center bgblack gap-8 text-white overflow-hidden">
-
-      <div className="flex flex-row mx-auto h-screen">
-      <div className="flex-col w-fit">
-      <div className="flex flex-row justify-between mt-20 tradebox p-2 rounded-md">
-        <div className="">You are offering:</div>
+    <main className="z-10 flex min-h-screen flex-row items-center bgblack gap-8 text-white overflow-auto">
+      <div className="flex flex-row mx-auto w-[85%]">
+      <div className="flex mx-auto h-screen w-full">
+      <div className="flex-col w-full">
+      <div className="flex flex-row justify-between mt-20 insidebox p-2 rounded-md">
+        <div className="">You are getting:</div>
         <div className="">C$ 21 🛒</div>
       </div>
-      <div className="flex flex-col p-2 tradebox h-[85%] mt-3 rounded-md">
-      <input className="w-full px-2 m-1 mb-2 mx-auto rounded-md searchbg " placeholder="⌕ Search"></input>
-      <div className="grid grid-cols-1 overflow-auto gap-2 md:grid-cols-3">
-        {array.map((e) => 
-        <div className="w-32 h-32 insidebox rounded-md shadow-inner">{e}</div>
+      <div className="overflow-auto p-2 tradebox h-[85%] mt-3 rounded-md">
+      <input className="w-full px-2 m-1 mb-2 mx-auto rounded-md searchbg" placeholder="⌕ Search"></input>
+      <div className="grid gap-2 items-grid">
+        {fakeItems.map((e) => 
+          <div className={`${checkedGeneral ? `${checkedHeroes.includes(e.hero) && fakeItems.some(e => e.price < 100 && e.price > smallRange) ? 'flex flex-col h-full w-full pb-1 px-1 insidebox rounded-sm shadow-inner border-2 borderinsidebox hover:bg-zinc-500 hover:border-blue-500' : 'hidden'}` : 'flex flex-col h-full w-full px-1 insidebox rounded-sm shadow-inner border-2 borderinsidebox pb-1 hover:bg-zinc-500 hover:border-blue-500'}`}><Image src={e.imgurl} alt='img' width={120} height={80} className="mx-auto"></Image>
+            <button className={`${inCart.includes(`${e.id}`)? 'text-white justify-end redhoveraccent w-full rounded-md' : 'w-full text-white h-fit justify-end bg-zinc-600 w-full rounded-md transition ease-in-out delay-150 hover:bg-red-300'}`} onClick={addToCart} value={e.id}>ADD</button>
+          </div>
         )}
       </div>
       </div>
       </div>
 
-      <div className="flex flex-col gap-8 mt-20 mx-8 pt-8 p-1 h-[90%] filterbg rounded-md">
-        <button className="mx-auto rounded-md redaccent py-[10px] text-sm shadow-inner text-lg w-[65%]">TRADE</button>
-        <div className="text-center">Filter</div>
-        <div className="mx-auto flex flex-col  w-50 p-1 rounded-md text-center gap-4 md:w-62">
+      <div className="flex flex-col mx-5 rounded-md w-fit">
+        <button className="mt-20 mx-auto rounded-md redaccent py-[10px] text-sm shadow-inner text-lg w-[65%]">TRADE</button>
+        <div className="justify-between flex flex-col filterbg h-[85%] rounded-md p-2 mt-3 overflow-auto">
+        <div className="mx-auto flex flex-col p-1 mt-3 rounded-md text-center gap-4">
+          <div className="text-center">Filter</div>
           <div className="text-left flex flex-col w-full gap-4 divide-y-2">Price
             <div className="flex flex-row gap-6 mx-auto text-white">
-              <input className="rounded-md text-center w-28 searchbg "  placeholder="C $0" value={pricerange} onChange={zero}></input>
-              <input className="rounded-md text-center w-28 searchbg " placeholder="C $∞"></input>
+              <input className="rounded-md text-center w-14 searchbg sm:w-28"  placeholder="C $0" value={smallRange} onChange={zero}></input>
+              <input className="rounded-md text-center w-14 searchbg sm:w-28" placeholder="C $∞" value={bigRange} onChange={infinity}></input>
             </div>
           </div>
           <button onClick={()=> setFilterplus(!filterPlus)} className="justify-between flex flex-row"><div className="">Heroes</div><div>{filterPlus? '-' : '+'}</div></button>
@@ -155,23 +154,25 @@ export default function Home() {
             <div className="text-red-400">Exotic</div>
           </div>
         </div>
-        <button onClick={clearFilterButton}>Clear</button>
+        <button onClick={clearFilterButton} className="mx-auto border-2 border-red-400 px-6 rounded-md">Clear</button>
+        </div>
         <button onClick={handleSendToApiFilter}></button>
       </div>
 
-      <div className=" flex-col">
+      <div className="flex-col w-full">
       <div className="flex flex-row justify-between mt-20 insidebox p-2 rounded-md">
         <div className="">You are getting:</div>
         <div className="">C$ 21 🛒</div>
       </div>
-      <div className="flex flex-col overflow-auto p-2 tradebox h-[85%] mt-3 rounded-md">
-      <input className="w-full px-2 m-1 mb-2 mx-auto rounded-md searchbg " placeholder="⌕ Search"></input>
-      <div className="grid overflow-auto gap-2 grid-cols-1 md:grid-cols-3">
+      <div className="overflow-auto p-2 tradebox h-[85%] mt-3 rounded-md">
+      <input className="w-full px-2 m-1 mb-2 mx-auto rounded-md searchbg" placeholder="⌕ Search"></input>
+      <div className="grid gap-2 items-grid">
         {fakeItems.map((e) => 
-          <div className={`${checkedGeneral ? `${checkedHeroes.includes(e.hero) ? 'w-32 h-32 px-1 insidebox rounded-sm shadow-inner border-2 borderinsidebox hover:bg-zinc-500 hover:border-blue-500' : 'hidden'}` : 'w-32 h-32 px-1 insidebox rounded-sm shadow-inner border-2 borderinsidebox hover:bg-zinc-500 hover:border-blue-500'}`}><Image src={e.imgurl} alt='img' width={90} height={90} className="mx-auto"></Image>
-            <button className={`${inCart.includes(`${e.id}`)? 'text-white justify-end redhoveraccent w-full rounded-md' : 'text-white justify-end bg-zinc-600 w-full rounded-md transition ease-in-out delay-150 hover:bg-red-300'}`} onClick={addToCart} value={e.id}>🛒</button>
+          <div className={`${checkedGeneral ? `${checkedHeroes.includes(e.hero) && fakeItems.some(e => e.price < 100 && e.price > smallRange) ? 'flex flex-col h-full w-full pb-1 px-1 insidebox rounded-sm shadow-inner border-2 borderinsidebox hover:bg-zinc-500 hover:border-blue-500' : 'hidden'}` : 'flex flex-col h-full w-full px-1 insidebox rounded-sm shadow-inner border-2 borderinsidebox pb-1 hover:bg-zinc-500 hover:border-blue-500'}`}><Image src={e.imgurl} alt='img' width={120} height={80} className="mx-auto"></Image>
+            <button className={`${inCart.includes(`${e.id}`)? 'text-white justify-end redhoveraccent w-full rounded-md' : 'w-full text-white h-fit justify-end bg-zinc-600 w-full rounded-md transition ease-in-out delay-150 hover:bg-red-300'}`} onClick={addToCart} value={e.id}>🛒</button>
           </div>
         )}
+      </div>
       </div>
       </div>
       </div>
